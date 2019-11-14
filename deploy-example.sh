e61c15aa5f40004876f7fba7dc533693723094ac
Tomucha <tomucha@gmail.com>
2019-11-14 12:10:38 +0100

    Deply script.

--- deploy-example.sh
+++ deploy-example.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 webdev build -o example:build &&
 cd build &&
-gcloud --project ma-web compute scp * static-ma:/usr/share/nginx/www/demo.fnx.io/fnx_gallery --zone europe-west1-b;
+gcloud --project ma-web compute scp * static-ma:/usr/share/nginx/www/demo.fnx.io/fnx_gallery --zone europe-west1-b --recurse
